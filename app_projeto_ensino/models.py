b507ef8f81a60b7109c974e00b9007ed6b9740b1
Melao12125 <MatheusForte12125@gmail.com>
2024-10-02 16:58:45 -0300

    update

--- app_projeto_ensino/models.py
+++ app_projeto_ensino/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    #email = models.EmailField(null=False, unique=True, blank=False) 
+    # blank so serve se for usar model form para setar como required
+    pontuacao = models.IntegerField(default=0)
+    first_name = None
+    last_name = None
+    # setar email como unique
+
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self): 
+        return self.username
+
+
--- app_projeto_ensino/views.py
+++ app_projeto_ensino/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def signup(request):
+
+    if request.method == "POST": 
+        
+
+        email = request.POST.get('email')
+        username = request.POST.get('usuario')
+        password = request.POST.get('senha')
+        password_confirm = request.POST.get('confirma_senha')
+
+        if password == password_confirm:
+            user = models.CustomUser.objects.create_user(username, email, password)
+            user.save()
+        else:
+            print("Senhas não coincidem")
+
+
+
+
+        print(request.POST.get('email'))
+        print(request.POST.get('usuario'))
+        print(request.POST.get('senha'))
+        print(request.POST.get('confirma_senha'))
 
     return render(request, 'signup.html')
 
