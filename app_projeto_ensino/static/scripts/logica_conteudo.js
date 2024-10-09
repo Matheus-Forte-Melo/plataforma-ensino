ad88c556535440b736704ba5105c94cc17758961
Melao12125 <MatheusForte12125@gmail.com>
2024-10-09 14:58:42 -0300

    update

--- app_projeto_ensino/static/scripts/logica_conteudo.js
+++ app_projeto_ensino/static/scripts/logica_conteudo.js
@@ -3,7 +3,7 @@ console.log(fase_inicial);
 let fase_atual = fase_inicial;
 fases = document.getElementsByClassName("fase")
 
-atualizarFases()
+atualizarFases() // Ja atualiza no começo, depois atualiza quando clicamos para passar. Tudo usa como base o django o valor no tempalte do django, então uma dessincronizaçao é improvavel
 
 function atualizarFases() {
     Array.from(fases).forEach(fase => {
@@ -11,30 +11,55 @@ function atualizarFases() {
         // node_fase contém o número da fase
         num_fase = Number(fase.getAttribute('data-fase'))
         conteudoFase = document.getElementById("conteudo" + num_fase)
-        let estadoFase;
-
-        if (num_fase < fase_atual) {
-            estadoFase = 'Desbloqueada';
-        } else if (num_fase === fase_atual) {
-            estadoFase = 'Atual';
-        } else {
-            estadoFase = 'Bloqueada';
-        }
+        let estadoFase = pegarEstadoFase(num_fase, fase_atual);
 
         if (estadoFase == "Desbloqueada") {
-            //fase.style.backgroundColor = "teal"
+            atualizarCor(fase)
             conteudoFase.innerHTML += '<br> Você concluiu essa fase!'
         } else if (estadoFase == "Atual") {
-            //fase.style.backgroundColor = "blue"
+            atualizarCor(fase)
             conteudoFase.innerHTML += '<button onclick="incrementarFase(this)">Desbloquear proxima fase.</button>'
         } else if (estadoFase == "Bloqueada") {
-            //fase.style.backgroundColor = "red"
+            fase.style.backgroundColor = "grey"
+            fase.style.outlineColor = "grey"
+            document.querySelector('.prova-icon').classList.add('bloqueado'); // Workaround fudido
         }
             
     });
 }
 
+function pegarEstadoFase(num_fase, fase_atual) {
+    if (num_fase < fase_atual) {
+        return 'Desbloqueada';
+    } else if (num_fase === fase_atual) {
+        return 'Atual';
+    } else {
+        return 'Bloqueada';
+    }
+}
 
+function atualizarCor(fase) {
+    if (fase.classList[1] == "conteudo-icon") {
+        fase.style.backgroundColor = "#84D260"
+        fase.style.outlineColor = "#84D260"
+    } 
+    
+    if (fase.classList[1] == "desafio-icon") {
+        fase.style.backgroundColor = "#ECA72C"
+        fase.style.outlineColor = "#ECA72C"
+    } 
+    
+    if (fase.classList[1] == "exercicio-icon") {
+        fase.style.backgroundColor = "#C02666"
+        fase.style.outlineColor = "#C02666"
+    } 
+    
+    if (fase.classList[1] == "prova-icon") {
+        fase.classList.remove('bloqueado')
+        fase.style.backgroundColor = "#EE5622"
+        fase.style.outlineColor = "#EE5622"
+    }
+}
 
 // Função que serve para pegar o cookie com o token csrf, porque o django é cu doce
 function getCookie(name) {
@@ -67,6 +92,4 @@ function incrementarFase(botao) {
         console.log(fase_atual)
         atualizarFases()
     })
-    
-   
 }
