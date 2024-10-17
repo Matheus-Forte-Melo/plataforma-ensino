8f0a1213e1eddf41683740254f2f7e9786e90711
Matheus Forte de Melo <114398115+Matheus-Forte-Melo@users.noreply.github.com>
2024-10-16 21:19:52 -0300

    Update settings.py

--- projeto_ensino/settings.py
+++ projeto_ensino/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,14 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = “/staticfiles/” 
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, “staticfiles_build”, “static”) 
 
-# Diretório onde o Django coletará os arquivos estáticos para o deploy
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Diretórios adicionais para arquivos estáticos durante o desenvolvimento
-STATICFILES_DIRS = [BASE_DIR / 'app_projeto_ensino' / 'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, “static”)]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
