9e4ec1919c0a5bfd4931fcf75fa2f4e8c2dc2150
Matheus Forte de Melo <114398115+Matheus-Forte-Melo@users.noreply.github.com>
2024-10-16 21:02:22 -0300

    Update settings.py

--- projeto_ensino/settings.py
+++ projeto_ensino/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'app_projeto_ensino/static/'
 
 # Diretório onde o Django coletará os arquivos estáticos para o deploy
 STATIC_ROOT = BASE_DIR / 'staticfiles'
