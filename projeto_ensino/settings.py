c56064d0ada1b5c7891ef74aea7108eccd79c124
Melao12125 <MatheusForte12125@gmail.com>
2024-10-18 11:30:55 -0300

    Update settings.py

--- projeto_ensino/settings.py
+++ projeto_ensino/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = 'django-insecure-r9t6cchd+nc0^&76jnvwfm$z6=%fgqdm0a+0x((88f@k4hu+wf
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['plataforma-ensino-bx5vtkpqd-matheus-forte-melos-projects.vercel.app', 'localhost', '127.0.0.1', 'vercel.app']
+
 
 
 # Application definition
